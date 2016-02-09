63d8eeda464a1f10ba149841b93bd2b5e42845ce
Christopher Kwai-Pun <chris.kwaipun@gmail.com>
2016-02-08 22:09:03 -0500

    pls

--- map.js
+++ map.js
@@ -1,22 +1,24 @@
 var map;
 
-require(["esri/map",
-         "esri/layers/FeatureLayer",
-         "esri/dijit/HomeButton",
-         "esri/dijit/Legend",
-         "esri/dijit/Scalebar",
-         "dojo/domReady!"],
- function(Map, FeatureLayer, HomeButton, Legend, Scalebar) {
-  map = new Map("map", {
-    basemap: "topo",
-    center: [-80.49,43.45],
-    zoom: 15
-  });
-  var points = new FeatureLayer('https://services1.arcgis.com/DwLTn0u9VBSZvUPe/arcgis/rest/services/w16gp487sidewalkissues/FeatureServer/0');
-  var home = new HomeButton({map:map},"HomeButton");
-  var scalebar = new Scalebar({map:map, scalebarUnit: 'metric', attatchTo: 'bottom-left'});
-  map.addLayer(points);
-  home.startup();
+require([
+  "esri/map",
+  "esri/arcgis/utils",
+  "esri/dijit/Legend",
+  "esri/dijit/Scalebar",
+  "dojo/domReady!"],
+  function (Map, arcgisUtils, Legend, Scalebar) {
+      arcgisUtils.createMap("91f1688adcc149d79af8813b5f9e914e", "map").then(function (response) {
+          map = response.map;
 
-  // var legend;
-});
+          var legend = new Legend({
+              map: map,
+              layerInfos:(arcgisUtils.getLegendLayers(response))
+          }, "legend");
+          var home = new HomeButton({map:map},"HomeButton");
+          var scalebar = new Scalebar({
+              map:map,
+              scalebarUnit: 'metric',
+              attatchTo: 'bottom-left'});
+          legend.startup();
+      });
+  });
