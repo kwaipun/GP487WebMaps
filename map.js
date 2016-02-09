b44740ae4aad603a5a64e0adc2061c3bf68b9f21
Christopher Kwai-Pun <chris.kwaipun@gmail.com>
2016-02-08 22:47:32 -0500

    working points

--- map.js
+++ map.js
@@ -7,18 +7,19 @@ require([
   "esri/dijit/Scalebar",
   "dojo/domReady!"],
   function (Map, arcgisUtils, Legend, Scalebar) {
+      arcgisUtils.arcgisUrl = 'http://www.arcgis.com/sharing/rest/content/items';
       arcgisUtils.createMap("91f1688adcc149d79af8813b5f9e914e", "map").then(function (response) {
           map = response.map;
 
-          var legend = new Legend({
-              map: map,
-              layerInfos:(arcgisUtils.getLegendLayers(response))
-          }, "legend");
-          var home = new HomeButton({map:map},"HomeButton");
+        //   var legend = new Legend({
+        //       map: map,
+        //       layerInfos:(arcgisUtils.getLegendLayers(response))
+        //   }, "legend");
+        //   var home = new HomeButton({map:map},"HomeButton");
           var scalebar = new Scalebar({
               map:map,
               scalebarUnit: 'metric',
               attatchTo: 'bottom-left'});
-          legend.startup();
+        //   legend.startup();
       });
   });
